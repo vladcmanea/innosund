31f3d6e0c9711508ce54ac13ee947d66241c1cad
vma <vma@e-conomic.com>
2014-10-17 22:34:35 +0200

    Removing all node stuff, keeping it a simple website

--- server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-var http = require('http');
-
-var app = connect()
-	.use(connect.favicon('public/favicon.ico'))
-	.use(connect.static('public'))
-	.use(connect.directory('public'));
-
-http.createServer(app).listen(process.env.PORT || 5000);
